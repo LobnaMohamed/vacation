8cf5e3c6e906fd76f5fccb3a0970d04fa194ebc8
LobnaMohamed <lobna92_19@hotmail.com>
2017-07-30 15:54:03 +0300

    begin bootstrap styling

--- index.php
+++ index.php
@@ -12,75 +12,89 @@
 </head>
 <body>
 	<div class="container">
-	  <header class="row">
-	  	<h1 class="col-lg-9 pull-right">نموذج الاجـــــازة</h1>
-	  	<img class= "col-lg-2 logo pull-left" src="images/amoc.jpg">
-	  </header>
-	  
-	    <form action="add.php" method="POST">	    
-			<div class="right">
-			    <label for="name">الاســـــم</label>
-			    <input type="text" class="name" name="name" placeholder="Your name..">
-			   
-			    <label for="code">رقم القيد</label>
-			    <input type="text" class="code" name="code" placeholder="Your Code..">
-			    
-			    <label for="address">العنوان</label>
-			    <input type="text" class="address" name="address" placeholder="Your address..">
-
-				<label for="date" >التاريخ</label>
-				<br>
-				<input type="date" class="date" name="vacDate" placeholder="date.."><br><br>
-
-				<label for="duration" >مدة الاجازة:</label>
-				<input type="text" class="duration" name="duration" placeholder="duration..">
-				<input type="submit" value="ارسال">
+	    <header class="row text-center">
+	  	    <h1 class="col-lg-9 pull-right">نموذج الاجـــــازة</h1>
+	  	    <img class= "col-lg-2 logo pull-left" src="images/amoc.jpg">
+	    </header>	  
+	    <form action="add.php" method="POST">
+	    	<div class="row">	    
+			    <div class="col-lg-4">
+			    	<label for="address" >العنوان</label>
+			    	<input type="text" class="address" name="address" placeholder="Your address..">
+			    	<label for="topManager">الرئيس الاعلى</label>
+				    <select class="topManager" name="topManager" required>
+				    	<option selected disabled hidden style='display: none' value=''></option>
+			   		    <?php
+			   		    	$sql= "SELECT ID,emp_code,emp_name FROM t_data" ;
+			   		    	$stmt = $con->prepare($sql);
+							$stmt->execute();
+							$result = $stmt->fetchAll();
+			   		    	    foreach($result as $row){
+								    echo "<option value=" .$row['ID'].">" . $row['emp_code'] ."   ".$row['emp_name']. "</option>";
+								}
+			   		    ?>			    
+			   		</select>
+			   		<label for="duration" >مدة الاجازة:</label>
+					<input type="text" class="duration" name="duration" placeholder="duration..">	
+			    </div>
+			    <div class="col-lg-4">
+			    	<div class="form-group">
+						<label for="code">رقم القيد</label>
+				    	<input type="text" class="code form-control" name="code" placeholder="Your Code..">
+			    	</div>
+					<div class="form-group">
+						<label for="manager">المدير المباشر</label>
+					    <select class="manager form-control" name="manager">
+					    	<option selected disabled hidden style='display: none' value=''></option>
+				   		    <?php
+				   		    	$sql= "SELECT ID,emp_code,emp_name FROM t_data" ;
+				   		    	$stmt = $con->prepare($sql);
+								$stmt->execute();
+								$result = $stmt->fetchAll();
+				   		    	    foreach($result as $row){
+									    echo "<option value=" .$row['ID'].">" . $row['emp_code'] ."   ".$row['emp_name']. "</option>";
+									}
+				   		    ?>
+						</select>
+					</div>
+			    	<div class="form-group">
+				    	<label for="date" >التاريخ من</label>
+						<input type="date" class="date form-control" name="vacDate" placeholder="date..">
+					</div>
+					<div class="form-group">	
+						<label for="dateTo" >التاريخ الى</label>
+						<input type="date" class="dateTo form-control" name="vacDateTo" placeholder="date to..">
+			    	</div>
+				</div> 					
+				<div class="col-lg-4">
+					<label for="name" >الاســـــم</label>
+			    	<input type="text" class="name " name="name" placeholder="Your name..">
+			    	<label for="Management" >الادارة:</label>
+					<input type="text" class="Management" name="Management" placeholder="Management..">
+					<label for="vacation">نوع الاجازة</label>
+		   		    <select class="vacType" name="case">
+		   		    	<option selected disabled hidden style='display: none' value=''></option>
+			   		    <?php
+			   		    	$sql= "SELECT ID,case_desc FROM t_case" ;
+			   		    	$stmt = $con->prepare($sql);
+							$stmt->execute();
+							$result = $stmt->fetchAll();
+			   		    	    foreach($result as $row){
+								    echo "<option value=" .$row['ID'].">" . $row['case_desc'] . "</option>";
+								}
+			   		    ?>
+				    </select>		
+				</div>
 			</div>
-			<div class="left">
+
+			<div class = "row">
+				<div class="col-lg-6">
+					<input class ="btn btn-success" type="Submit" value="ارسال">
+					<!-- <i class="fa fa-send fa-fw send-icon"></i>			 -->
+				</div>	
+			</div>
 				
-				<label for="Management" >الادارة:</label>
-				<input type="text" class="Management" name="Management" placeholder="Management..">
-				<label for="manager">المدير المباشر</label>
-			    <select class="manager" name="manager">
-			    <option selected disabled hidden style='display: none' value=''></option>
-		   		    <?php
-		   		    	$sql= "SELECT ID,emp_code,emp_name FROM t_data" ;
-		   		    	$stmt = $con->prepare($sql);
-						$stmt->execute();
-						$result = $stmt->fetchAll();
-		   		    	    foreach($result as $row){
-							    echo "<option value=" .$row['ID'].">" . $row['emp_code'] ."   ".$row['emp_name']. "</option>";
-							}
-		   		    ?>
-				</select>
-			    <label for="topManager">الرئيس الاعلى</label>
-			    <select class="topManager" name="topManager" required>
-			    <option selected disabled hidden style='display: none' value=''></option>
-		   		    <?php
-		   		    	$sql= "SELECT ID,emp_code,emp_name FROM t_data" ;
-		   		    	$stmt = $con->prepare($sql);
-						$stmt->execute();
-						$result = $stmt->fetchAll();
-		   		    	    foreach($result as $row){
-							    echo "<option value=" .$row['ID'].">" . $row['emp_code'] ."   ".$row['emp_name']. "</option>";
-							}
-		   		    ?>			    
-		   		</select>
 
-			    <label for="vacation">نوع الاجازة</label>
-	   		    <select class="vacType" name="case">
-	   		    <option selected disabled hidden style='display: none' value=''></option>
-		   		    <?php
-		   		    	$sql= "SELECT ID,case_desc FROM t_case" ;
-		   		    	$stmt = $con->prepare($sql);
-						$stmt->execute();
-						$result = $stmt->fetchAll();
-		   		    	    foreach($result as $row){
-							    echo "<option value=" .$row['ID'].">" . $row['case_desc'] . "</option>";
-							}
-		   		    ?>
-			    </select>
-			</div>
 	    </form>
 	</div>
 	<script src="js/jquery-3.2.1.min.js"></script>
